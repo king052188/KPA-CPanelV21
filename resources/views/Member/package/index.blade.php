8bd5c29467f17b7cdeddd08d8482f48097ee698b
king052188 <me@kpa21.info>
2017-05-02 09:24:02 +0800

    Changed Package Plan Layout

--- resources/views/Member/package/index.blade.php
+++ resources/views/Member/package/index.blade.php
@@ -5,6 +5,26 @@
     $url_secured = $helper["status"];
     ?>
 
+    <script>
+        var last_id = 0;
+        var choose_id = 0;
+        function event_click(id) {
+            console.log(id);
+            $(document).ready(function() {
+                if(last_id == 0) {
+                    last_id = parseInt(id);
+                }
+                else {
+                    $('#btnChoose_'+last_id).removeAttr("class");
+                    $('#btnChoose_'+last_id).attr("class", "btn btn-warning");
+                    last_id = parseInt(id);
+                }
+                $('#btnChoose_'+id).removeAttr("class");
+                $('#btnChoose_'+id).attr("class", "btn btn-success");
+                choose_id = parseInt(id);
+            })
+        }
+    </script>
     <!--banner-->
     <div class="banner">
         <h2>
@@ -50,6 +70,41 @@
                 <span id="package_info_msg" class="checking"></span>
             </div>
 
+            <link rel="stylesheet" href="{{ asset('css/table-responsive.css') }}">
+            <script src="http://zurb.com/playground/projects/responsive-tables/responsive-tables.js"></script>
+
+            <div class="col-md-12 form-group1 group-mail">
+                <table id="packages_plan" class="responsive" style="width: 100%;">
+                    <tr>
+                        <th style="text-align: right; width: 190px; border-bottom: 1px solid #F0F0F0;">Package</th>
+                        <th style="text-align: center; width: 150px; border-bottom: 1px solid #F0F0F0;">Web Site</th>
+                        <th style="text-align: center; width: 150px; border-bottom: 1px solid #F0F0F0;">MySQL</th>
+                        <th style="text-align: center; width: 150px; border-bottom: 1px solid #F0F0F0;">FTP</th>
+                        <th style="text-align: center; width: 150px; border-bottom: 1px solid #F0F0F0;">Storage</th>
+                        <th style="text-align: center; width: 150px; border-bottom: 1px solid #F0F0F0;">Bandwidth</th>
+                        <th style="text-align: center; width: 150px; border-bottom: 1px solid #F0F0F0;">DDoS Protection</th>
+                        <th id="usd_price_th" style="text-align: center; border-bottom: 1px solid #F0F0F0;">USD | <a href="#" id="show_php">PHP</a></th>
+                        <th id="php_price_th" style="text-align: center; border-bottom: 1px solid #F0F0F0; display: none;"><a href="#" id="show_usd">USD</a> | PHP</th>
+                        <th style="text-align: center; width: 150px; border-bottom: 1px solid #F0F0F0;">Action</th>
+                    </tr>
+                    @for($i = 0; $i < COUNT($packages); $i++)
+                        <tr>
+                            <td style="text-align: right; border-bottom: 1px solid #F0F0F0;">{{ $packages[$i]->code_name }}</td>
+                            <td style="text-align: center; border-bottom: 1px solid #F0F0F0;">{{ $packages[$i]->web }}</td>
+                            <td style="text-align: center; border-bottom: 1px solid #F0F0F0;">{{ $packages[$i]->mysql }}</td>
+                            <td style="text-align: center; border-bottom: 1px solid #F0F0F0;">{{ $packages[$i]->ftp }}</td>
+                            <td style="text-align: center; border-bottom: 1px solid #F0F0F0;">{{ number_format($packages[$i]->disk, 2) }} GB</td>
+                            <td style="text-align: center; border-bottom: 1px solid #F0F0F0;">Unmetered</td>
+                            <td style="text-align: center; border-bottom: 1px solid #F0F0F0;">YES</td>
+                            <td id="usd_price_{{ $i }}" style="text-align: right; border-bottom: 1px solid #F0F0F0;">{{ number_format($packages[$i]->price_usd, 2) }}<span style="font-size: 13px; color: #C5C5C5;"> /month</span></td>
+                            <td id="php_price_{{ $i }}" style="text-align: right; border-bottom: 1px solid #F0F0F0; display: none;">{{ number_format($packages[$i]->price_ph, 2) }}<span style="font-size: 13px; color: #C5C5C5;"> /month</span></td>
+                            <td style="text-align: center; border-bottom: 1px solid #F0F0F0;"><button type="submit" onclick="event_click({{ $packages[$i]->Id }})" id="btnChoose_{{ $packages[$i]->Id }}" class="btn btn-warning">Choose</button></td>
+                        </tr>
+                    @endfor
+
+                </table>
+            </div>
+
             <div class="clearfix"> </div>
 
             <div class="col-md-12 form-group">
@@ -87,6 +142,26 @@
     <script>
         var package ;
         $(document).ready(function() {
+
+            var total_packages = {{ COUNT($packages) }};
+            $( "#show_php" ).click(function() {
+                $( "#usd_price_th" ).hide();
+                $( "#php_price_th" ).show();
+                for(var i = 0; i < total_packages; i++){
+                    $( "#usd_price_" + i ).hide();
+                    $( "#php_price_" + i ).show();
+                }
+            });
+
+            $( "#show_usd" ).click(function() {
+                $( "#usd_price_th" ).show();
+                $( "#php_price_th" ).hide();
+                for(var i = 0; i < total_packages; i++){
+                    $( "#php_price_" + i ).hide();
+                    $( "#usd_price_" + i ).show();
+                }
+            });
+
             $( "#packages" ).change(function() {
                 package = $( "#packages" ).val();
                 if(package == "0") {
